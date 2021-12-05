8e55493a7fb7594832ac81977935b2ab10427fc5
Mikhail Brinchuk <thecentury@gmail.com>
2021-12-05 19:27:36 +0100

    Added packages and reproduce a failure

--- Program.cs
+++ Program.cs
@@ -6,6 +6,17 @@ namespace FaceRecognitionRepro
     {
         static void Main(string[] args)
         {
+            // The next line throws an exception:
+            
+            // Unhandled exception. System.TypeInitializationException: The type initializer for 'DlibDotNet.NativeMethods' threw an exception.
+            // ---> System.DllNotFoundException: Unable to load shared library 'DlibDotNetNativeDnn' or one of
+            //     its dependencies.In order to help diagnose loading problems, consider setting the DYLD_PRINT_LIBRARIES
+            //     environment variable:
+            // dlopen(libDlibDotNetNativeDnn, 1): image not found
+            using (var detector = DlibDotNet.Dlib.GetFrontalFaceDetector())
+            {
+            }
+
             Console.WriteLine("Hello World!");
         }
     }
